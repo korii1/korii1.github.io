274658d1bcd3286379ad1ba3cce3c9aa76aff3ab
kori <owo.korii@gmail.com>
2026-09-20 12:20:14 -0300

    V1.0.0

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// SPLASH TEXT
+async function loadSplash() {
+	const response = await fetch("/splash_texts.json");
+	const texts = await response.json();
+
+	const splash = document.getElementById("splash");
+	const length = texts.length;
+	let splash_index = 0;
+
+	function animate() {
+		const txt = texts[splash_index];
+		splash_index = (splash_index + 1) % length;
+		splash.textContent = "you are.. " + txt;
+	}
+
+	animate();
+	setInterval(animate, 670);
+}
+loadSplash();
